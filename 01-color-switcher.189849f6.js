c97ebd8a957d8711980acb437a9113b6e1992ff5
zsribna <73397900+zsribna@users.noreply.github.com>
2023-08-08 12:11:58 +0000

    Deploying to gh-pages from @ zsribna/goit-js-hw-09@06a800848ca72f8d433fb3f2e9b756a1de886ef3 🚀

---
+++ 01-color-switcher.189849f6.js
@@ -0,0 +1,2 @@
+const t=document.querySelector("button[data-start]"),e=document.querySelector("button[data-stop]"),d=document.querySelector("body");let a=null;t.addEventListener("click",(()=>{t.setAttribute("disabled","disabled"),e.removeAttribute("disabled","disabled"),a=setInterval((()=>{d.style.backgroundColor=`#${Math.floor(16777215*Math.random()).toString(16).padStart(6,0)}`}),1e3)})),e.addEventListener("click",(()=>{t.removeAttribute("disabled","disabled"),e.setAttribute("disabled","disabled"),clearInterval(a)}));
+//# sourceMappingURL=01-color-switcher.189849f6.js.map
